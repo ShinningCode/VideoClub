f854878de542bbf0cf2dd2220809a744ef176f2c
Edgar Gonzalez <a329927@uach.mx>
2023-04-19 18:51:26 -0600

    Se cargan cambios en fecha de 19/04/2023 En estos archivos: -modified:   controllers/directors.js -modified:   controllers/members.js -modified:   controllers/movies.js

--- controllers/directors.js
+++ controllers/directors.js
@@ -1,100 +1,99 @@
 const express = require('express');
-const Director = require('../models/directors');
-/* GET users listing. */
+const Director = require('../models/director');
 
-function list(req,res,next){
-  Director.find().then(objs=>res.status(200).json({
-    message: "Director's list",
-    obj: objs
-  })).cathc(ex => res.status(500).json({
-    message: "Query could not be performed",
-    obj: ex
-  }));
+
+function list(req, res, next){
+    Director.find().then(objs => res.status(200).json({
+        message:"lista de directores",
+        obj:objs
+    })).catch(ex => res.status(500).json({
+        message:"No se pudo consultar la información",
+        obj:ex
+    }));
 };
 
-function index(req,res,next){
-  const id = req.params.id;
-  Director.findOne({"_id":id}).then(obj => res.status(200).json({
-      message: `Director with id ${id}`,
-      obj:obj
-  })).catch(ex=>res.status(500).json({
-      message: `Query could not be performed`,
-      obj:ex
-
-  })); 
-}
+function index(req, res, next){
+    const id = req.params.id;
+    Director.findOne({"_id":id}).then(obj=>res.status(200).json({
+        message:`Director con id ${id}`,
+        obj: obj
+    })).catch(ex => res.status(500).json({
+            message:"No se pudo consultar la información",
+            obj:ex
+        }));
+};
 
 function create(req, res, next){
-  let name = req.body.name;
-  let lastName = req.body.lastName;
-  
-  let director = new Director({
-      name:name,
-      lastName:lastName
-  });
+    let name = req.body.name;
+    let lastName = req.body.lastName;
+    
+    let director = new Director({
+        name:name,
+        lastName:lastName
+    });
 
-  director.save().then(obj => res.status(200).json({
-      message:"Director created successfully",
-      obj:obj
-  })).catch(ex => res.status(500).json({
-      message:"Could not store director",
-      obj:ex
-  }));
+    director.save().then(obj => res.status(200).json({
+        message:"Director creado correctamente",
+        obj:obj
+    })).catch(ex => res.status(500).json({
+        message:"No se pudo almacenar el director",
+        obj:ex
+    }));
 };
 
 function replace(req, res, next){
-  const id = req.params.id;
-  let name = req.body.name ? req.body.name : "";
-  let lastName = req.body.lastName ? req.body.lastName : "";
+    const id = req.params.id;
+    let name = req.body.name ? req.body.name : "";
+    let lastName = req.body.lastName ? req.body.lastName : "";
 
-  let director = new Object({
-      _name : name,
-      _lastName : lastName
-  });
+    let director = new Object({
+        _name : name,
+        _lastName : lastName
+    });
 
-  Director.findOneAndUpdate({"_id":id},director,{new : true})
-          .then(obj => {res.status(200).json({
-              message: "Director updated successfully",
-              obj:obj
-          })}).catch(ex => res.status(500).json({
-              message:"Could not replace director",
-              obj:ex
-          }));
+    Director.findOneAndUpdate({"_id":id},director,{new : true})
+            .then(obj => {res.status(200).json({
+                message: "Director actualizado correctamente",
+                obj:obj
+            })}).catch(ex => res.status(500).json({
+                message:"No se pudo remplazar el director",
+                obj:ex
+            }));
 };
 
 function update(req, res, next){
-  const id = req.params.id;
-  let name = req.body.name;
-  let lastName = req.body.lastName;
+    const id = req.params.id;
+    let name = req.body.name;
+    let lastName = req.body.lastName;
 
-  let director = new Object();
+    let director = new Object();
 
-  if(name){
-      director._name = name;
-  }
-  if(lastName){
-      director._lastName = lastName;
-  }
+    if(name){
+        director._name = name;
+    }
+    if(lastName){
+        director._lastName = lastName;
+    }
 
-  Director.findOneAndUpdate({"_id":id},director)
-          .then(obj => res.status(200).json({
-              message:"Director updated successfully",
-              obj:obj
-          })).catch(ex => res.status(500).json({
-              message:"Could not replace director",
-              obj:ex
-          }));
+    Director.findOneAndUpdate({"_id":id},director)
+            .then(obj => res.status(200).json({
+                message:"Director actuaizado correctamente",
+                obj:obj
+            })).catch(ex => res.status(500).json({
+                message:"No se pudo remplazar el director",
+                obj:ex
+            }));
 };
 
 function destroy(req, res, next){
-  const id = req.params.id;
-  Director.findByIdAndRemove({"_id":id}).then(obj => res.status(200).json({
-      message: "Director removed successfully",
-      obj:obj
-  })).catch(ex => res.status(500).json({
-      message:"Could not remove director",
-      obj:ex
-  }));
+    const id = req.params.id;
+    Director.findByIdAndRemove({"_id":id}).then(obj => res.status(200).json({
+        message: "Director eliminado correctamente",
+        obj:obj
+    })).catch(ex => res.status(500).json({
+        message:"No se pudo eliminar el director",
+        obj:ex
+    }));
 };
 
 module.exports = {list,index,create,update,destroy,replace};
